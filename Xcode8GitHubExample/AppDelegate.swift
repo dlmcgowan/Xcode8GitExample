7a2d2b36fe44fc5ad0636fc6bb45af1e1340f286
David McGowan <davidmcgowan@Davids-Mac-mini.local>
2016-12-07 22:07:34 -0500

    Initial Commit

---
+++ Xcode8GitHubExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8GitHubExample
+//
+//  Created by David McGowan on 12/7/16.
+//  Copyright © 2016 David McGowan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8GitHubExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitHubExample
+//
+//  Created by David McGowan on 12/7/16.
+//  Copyright © 2016 David McGowan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
